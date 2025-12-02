b9ac81916fad4615c402ffd0a37987534d51f261
Keane Grech <keanegrech@gmail.com>
2025-12-02 13:06:12 +0000

    provisional setup of models

---
+++ database/migrations/2025_12_02_130032_create_games.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('steam_app_id')->nullable();
+            $table->string('developer');
+            $table->integer('percent_complete');
+            $table->date('release_date')->nullable();
+            $table->foreignId('machine_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
