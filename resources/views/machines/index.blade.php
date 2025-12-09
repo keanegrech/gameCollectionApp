eb7b835e355e3657824c43ab91f088d81f80bd9d
Keane Grech <keanegrech@gmail.com>
2025-12-09 21:18:59 +0000

    table view for machines

--- resources/views/machines/index.blade.php
+++ resources/views/machines/index.blade.php
@@ -2,8 +2,46 @@
 
 @section('content')
 
-<h1>Machines</h1>
+<div class="container-fluid p-3">
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h1 class="m-0">Machines</h1>
+        <a href="#" class="btn btn-success">
+            <i class="fa fa-plus"></i> Add New Machine
+        </a>
+    </div>
 
-{{ $machines }}
+    <div class="table-responsive">
+        <table class="table table-striped table-hover table-bordered align-middle">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col" class="text-center" style="width: 50px;">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Manufacturer</th>
+                    <th scope="col" style="width: 150px;">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($machines as $machine)
+                <tr>
+                    <th scope="row" class="text-center fw-normal">{{ $machine->id }}</th>
+                    <td>{{ $machine->name }}</td>
+                    <td>{{ $machine->manufacturer }}</td>
+                    <td style="white-space: nowrap;">
+                        <a href="#" class="btn btn-sm btn-primary">
+                            <i class="fa fa-eye"></i> View
+                        </a>
+                        <a href="#" class="btn btn-sm btn-secondary">
+                            <i class="fa fa-edit"></i> Edit
+                        </a>
+                        <a href="#" class="btn btn-sm btn-danger">
+                            <i class="fa fa-times"></i> Delete
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 @endsection
