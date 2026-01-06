6d5721ec7b88d3322bf9a16f517c2a1defeedad4
Keane Grech <keanegrech@gmail.com>
2026-01-06 16:31:33 +0000

    Add welcome message and description to the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 <div class="container m-4">
+    <h1>
+        <strong>
+            Welcome to the Game Collection App
+        </strong>
+    </h1>
+    <h2>Organize your games and machines with ease</h2>
     <a href="{{ route('machines.index') }}" class="btn btn-primary mb-3 ms-2">
         Machines
     </a>
